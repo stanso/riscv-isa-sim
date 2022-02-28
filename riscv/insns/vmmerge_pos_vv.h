a00d061302853f918283de24edac2ae1f0d3d2bd
Tianshuo Su <ts_su@outlook.com>
2022-02-27 23:58:17 -0600

    bug fix in vmmerge_pos_vv.h

--- riscv/insns/vmmerge_pos_vv.h
+++ riscv/insns/vmmerge_pos_vv.h
@@ -1,41 +1,46 @@
-// vmmerge_pos.vv vd, vs2, vs1
+// vmmerge_pos.vv pos_mask, stream_2, stream_1
+// OPIVV
+
 require(P.VU.vsew >= e8 && P.VU.vsew <= e64);
 require_vector(true);
+
 reg_t vl = P.VU.vl->read();
 reg_t sew = P.VU.vsew;
-reg_t rd_num = insn.rd();
-reg_t rs1_num = insn.rs1();
-reg_t rs2_num = insn.rs2();
+reg_t rd_num = insn.rd(); // dest position mask
+reg_t rs1_num = insn.rs1(); // stream source vreg 1
+reg_t rs2_num = insn.rs2(); // stream source vreg 2
 
+// Get vmmergeposcolli flag register (TODO: may sqeeze to a bit of other registers)
 int csr = validate_csr(CSR_VMMERGEPOSCOLLI, true);
 
 reg_t cur_vs1_idx = 0, cur_vs2_idx = 0;
 for (reg_t i = P.VU.vstart->read(); i < vl; ++i)
 {
   VI_LOOP_ELEMENT_SKIP();
-  uint64_t mmask = UINT64_C(1) << mpos; \
-  uint64_t &vdi = P.VU.elt<uint64_t>(insn.rd(), midx, true); \
+  uint64_t mmask = UINT64_C(1) << mpos;
+  uint64_t &vdi = P.VU.elt<uint64_t>(insn.rd(), midx, true);
   uint64_t res = 0;
 
   uint64_t vs1, vs2;
 
+  // get the current element in the source vreg
   switch (sew)
   {
   case e8:
     vs1 = static_cast<uint64_t>(P.VU.elt<uint8_t>(rs1_num, cur_vs1_idx));
-    vs2 = static_cast<uint64_t>(P.VU.elt<uint8_t>(rs1_num, cur_vs2_idx));
+    vs2 = static_cast<uint64_t>(P.VU.elt<uint8_t>(rs2_num, cur_vs2_idx));
     break;
   case e16:
     vs1 = static_cast<uint64_t>(P.VU.elt<uint16_t>(rs1_num, cur_vs1_idx));
-    vs2 = static_cast<uint64_t>(P.VU.elt<uint16_t>(rs1_num, cur_vs2_idx));
+    vs2 = static_cast<uint64_t>(P.VU.elt<uint16_t>(rs2_num, cur_vs2_idx));
     break;
   case e32:
     vs1 = static_cast<uint64_t>(P.VU.elt<uint32_t>(rs1_num, cur_vs1_idx));
-    vs2 = static_cast<uint64_t>(P.VU.elt<uint32_t>(rs1_num, cur_vs2_idx));
+    vs2 = static_cast<uint64_t>(P.VU.elt<uint32_t>(rs2_num, cur_vs2_idx));
     break;
   default:
     vs1 = P.VU.elt<uint64_t>(rs1_num, cur_vs1_idx);
-    vs2 = P.VU.elt<uint64_t>(rs1_num, cur_vs2_idx);
+    vs2 = P.VU.elt<uint64_t>(rs2_num, cur_vs2_idx);
     break;
   }
 
@@ -60,6 +65,9 @@ for (reg_t i = P.VU.vstart->read(); i < vl; ++i)
     p->set_csr(csr, 1);
   }
 
-  vdi = (vdi & ~mmask) | (((res) << mpos) & mmask); \
+  // accumulate one bit in the result
+  vdi = (vdi & ~mmask) | (((res) << mpos) & mmask);
 }
+
+// reset vstart
 P.VU.vstart->write(0);
